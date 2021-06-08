f377c1f30efd3f295d16e7f2ea7648a387db34da
Nick R <nro1912@gmail.com>
2021-06-08 21:31:04 +0300

    work in progress

--- Assets/Scripts/ApplicationManager.cs
+++ Assets/Scripts/ApplicationManager.cs
@@ -1,9 +1,60 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
-public class ApplicationManager : MonoBehaviour {
-	
+public class ApplicationManager : MonoBehaviour
+{
+	[SerializeField] private Image pointer;
+	private Vector3 startPointerPosition;
+	private RectTransform pointerRT;
+	private int pointerX = 1;
+	private int pointerY = 1;
+	private const float stepX = 0.54f;
+	private const float stepY = -0.54f;
 
+	private void Start()
+    {
+		pointerRT = pointer.GetComponent<RectTransform>();
+		startPointerPosition = pointerRT.position;
+    }
+
+    private void SetPointerPosition()
+    {
+		float px = pointerX-1;
+		float py = pointerY-1;
+		Vector3 pos = new Vector3(px * stepX, py * stepY, 0);
+
+		pointerRT.position = startPointerPosition + pos;
+    }
+	public void UpPointer()
+    {
+		if (pointerY == 1) return;
+		pointerY--;
+		SetPointerPosition();
+
+	}
+	public void DownPointer()
+	{
+		if (pointerY == 9) return;
+		pointerY++;
+		SetPointerPosition();
+	}
+	public void LeftPointer()
+	{
+		if (pointerX == 1) return;
+		pointerX--;
+		SetPointerPosition();
+	}
+	public void RightPointer()
+	{
+		if (pointerX == 9) return;
+		pointerX++;
+		SetPointerPosition();
+	}
+	public void pressNum(int _num)
+    {
+		Debug.Log(_num);
+    }
 	public void Quit () 
 	{
 		#if UNITY_EDITOR
---
+++ Assets/Scripts/CCellCheck.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CCellCheck : MonoBehaviour
+{
+    public Vector3 pos;
+    private Vector3 startPosition;
+    private RectTransform cellRT;
+    private GameObject[] hints;
+    private bool[] freeNums;
+    private static int cellCounter = 0;
+    private int cellNum;
+
+    private void ViewHints()
+    {
+     int i;
+
+        for (i = 0; i < 9; i++)
+        {
+            hints[i].SetActive(freeNums[i+1]);
+        }
+    }
+
+    void Start()
+    {
+     int i;
+
+        cellNum = cellCounter++;
+        cellRT = GetComponent<RectTransform>();
+        pos = startPosition = cellRT.position;
+
+        freeNums = new bool[10];
+        for (i = 0; i < 10; i++) freeNums[i] = false;
+
+        hints = new GameObject[9];
+        for (i = 0; i < 9; i++)
+        {
+            hints[i] = transform.GetChild(i).gameObject;
+        }
+        ViewHints();
+    }
+
+    void Update()
+    {
+        
+    }
+}
